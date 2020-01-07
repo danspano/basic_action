de0695d37913245a93824b2e81e7942e8af7eeaf
danspano <danspano@users.noreply.github.com>
2020-01-08 09:11:09 +1100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
-    console.log(`Hello!`);
+    console.log('Hello!');
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
